fdcda41eba7d9c8ab262752e76a88cf9732749cd
ableaiden707@gmail.com <ableaiden707@gmail.com>
2022-08-11 17:08:55 +0900

    first

---
+++ brandnew/src/main/java/example/brandnew/BrandnewApplication.java
@@ -0,0 +1,13 @@
+package example.brandnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandnewApplication.class, args);
+	}
+
+}
